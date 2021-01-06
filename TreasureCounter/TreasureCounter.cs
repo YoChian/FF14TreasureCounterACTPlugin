d340158262382de04f11e9b4c10b6f4990681e5b
YoChian <591042812@qq.com>
2021-01-07 02:14:45 +0800

    添加项目文件。

---
+++ TreasureCounter/TreasureCounter.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace TreasureCounter
+{
+    public class PluginMain : UserControl,IActPluginV1
+    {
+        private CheckedListBox ItemList;
+        private Label label1;
+        private TextBox AddItemText;
+        private Button AddItemButton;
+        private Button RemoveButton;
+        private TextBox BaseSalaryText;
+        private Label label2;
+        private Label MapLabel;
+        private Label TPLabel;
+        private Label SalaryLabel;
+        private Button ResetButton;
+        private Button StartButton;
+
+        public PluginMain()
+        {
+            InitializeComponent();
+        }
+
+        Label lblStatus;    // The status label that appears in ACT's Plugin tab
+        string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\PluginSample.config.xml");
+        SettingsSerializer xmlSettings;
+
+        void IActPluginV1.InitPlugin(System.Windows.Forms.TabPage pluginScreenSpace, System.Windows.Forms.Label pluginStatusText)
+        {
+            lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
+            pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
+            this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
+            xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+            LoadSettings();
+            ItemList.Items.Clear();
+            // Create some sort of parsing event handler.  After the "+=" hit TAB twice and the code will be generated for you.
+            ActGlobals.oFormActMain.AfterCombatAction += new CombatActionDelegate(oFormActMain_AfterCombatAction);
+
+            lblStatus.Text = "就绪";
+        }
+
+        void IActPluginV1.DeInitPlugin()
+        {
+            // Unsubscribe from any events you listen to when exiting!
+            ActGlobals.oFormActMain.AfterCombatAction -= oFormActMain_AfterCombatAction;
+
+            SaveSettings();
+            lblStatus.Text = "离线";
+        }
+
+        void oFormActMain_AfterCombatAction(bool isImport, CombatActionEventArgs actionInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveSettings()
+        {
+            FileStream fs = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+            xWriter.Indentation = 1;
+            xWriter.IndentChar = '\t';
+            xWriter.WriteStartDocument(true);
+            xWriter.WriteStartElement("Config");    // <Config>
+            xWriter.WriteStartElement("SettingsSerializer");    // <Config><SettingsSerializer>
+            xmlSettings.ExportToXml(xWriter);   // Fill the SettingsSerializer XML
+            xWriter.WriteEndElement();  // </SettingsSerializer>
+            xWriter.WriteEndElement();  // </Config>
+            xWriter.WriteEndDocument(); // Tie up loose ends (shouldn't be any)
+            xWriter.Flush();    // Flush the file buffer to disk
+            xWriter.Close();
+        }
+
+        private void LoadSettings()
+        {
+            // Add any controls you want to save the state of
+            /*
+			xmlSettings.AddControlSetting(textBox1.Name, textBox1);
+
+			if (File.Exists(settingsFile))
+			{
+				FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+				XmlTextReader xReader = new XmlTextReader(fs);
+
+				try
+				{
+					while (xReader.Read())
+					{
+						if (xReader.NodeType == XmlNodeType.Element)
+						{
+							if (xReader.LocalName == "SettingsSerializer")
+							{
+								xmlSettings.ImportFromXml(xReader);
+							}
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					lblStatus.Text = "Error loading settings: " + ex.Message;
+				}
+				xReader.Close();
+			}
+            */
+        }
+
+        private void InitializeComponent()
+        {
+            this.ItemList = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddItemText = new System.Windows.Forms.TextBox();
+            this.AddItemButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.BaseSalaryText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MapLabel = new System.Windows.Forms.Label();
+            this.TPLabel = new System.Windows.Forms.Label();
+            this.SalaryLabel = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ItemList
+            // 
+            this.ItemList.FormattingEnabled = true;
+            this.ItemList.Location = new System.Drawing.Point(4, 4);
+            this.ItemList.Name = "ItemList";
+            this.ItemList.Size = new System.Drawing.Size(139, 196);
+            this.ItemList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "添加物品";
+            // 
+            // AddItemText
+            // 
+            this.AddItemText.Location = new System.Drawing.Point(6, 223);
+            this.AddItemText.Name = "AddItemText";
+            this.AddItemText.Size = new System.Drawing.Size(100, 21);
+            this.AddItemText.TabIndex = 2;
+            // 
+            // AddItemButton
+            // 
+            this.AddItemButton.Location = new System.Drawing.Point(113, 223);
+            this.AddItemButton.Name = "AddItemButton";
+            this.AddItemButton.Size = new System.Drawing.Size(59, 21);
+            this.AddItemButton.TabIndex = 3;
+            this.AddItemButton.Text = "添加";
+            this.AddItemButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(150, 22);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 4;
+            this.RemoveButton.Text = "移除";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // BaseSalaryText
+            // 
+            this.BaseSalaryText.Location = new System.Drawing.Point(149, 68);
+            this.BaseSalaryText.Name = "BaseSalaryText";
+            this.BaseSalaryText.Size = new System.Drawing.Size(100, 21);
+            this.BaseSalaryText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "底薪";
+            // 
+            // MapLabel
+            // 
+            this.MapLabel.AutoSize = true;
+            this.MapLabel.Location = new System.Drawing.Point(147, 92);
+            this.MapLabel.Name = "MapLabel";
+            this.MapLabel.Size = new System.Drawing.Size(65, 12);
+            this.MapLabel.TabIndex = 7;
+            this.MapLabel.Text = "已挖图数：";
+            // 
+            // TPLabel
+            // 
+            this.TPLabel.AutoSize = true;
+            this.TPLabel.Location = new System.Drawing.Point(147, 104);
+            this.TPLabel.Name = "TPLabel";
+            this.TPLabel.Size = new System.Drawing.Size(65, 12);
+            this.TPLabel.TabIndex = 8;
+            this.TPLabel.Text = "下洞次数：";
+            // 
+            // SalaryLabel
+            // 
+            this.SalaryLabel.AutoSize = true;
+            this.SalaryLabel.Location = new System.Drawing.Point(147, 116);
+            this.SalaryLabel.Name = "SalaryLabel";
+            this.SalaryLabel.Size = new System.Drawing.Size(65, 12);
+            this.SalaryLabel.TabIndex = 9;
+            this.SalaryLabel.Text = "当前工资：";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(266, 176);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 10;
+            this.ResetButton.Text = "重置";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(150, 176);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 11;
+            this.StartButton.Text = "开始";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // PluginMain
+            // 
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.SalaryLabel);
+            this.Controls.Add(this.TPLabel);
+            this.Controls.Add(this.MapLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BaseSalaryText);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddItemButton);
+            this.Controls.Add(this.AddItemText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ItemList);
+            this.Name = "PluginMain";
+            this.Size = new System.Drawing.Size(370, 297);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
